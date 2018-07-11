2d1abc5828e95bd5ba509fc3af37c2c3039b8400
mxito3 <1477311729@qq.com>
2018-07-11 11:52:53 +0800

    分离数据库代码

---
+++ blog/sql.py
@@ -0,0 +1,20 @@
+import pymysql
+
+class Sql:
+    def __init__(self,host,port,user,passwd,db):
+        self.host=host
+        self.port=port
+        self.user=user
+        self.passwd=passwd
+        self.db=db
+    def connect(self):
+        self.con=pymysql.connect(host=self.host,port=self.port,user=self.user,passwd=self.passwd,db=self.db)
+        self.cursor=self.con.cursor()
+    def close(self):
+        self.con.close()
+    def extractSql(self,command):
+        self.cursor.execute(command)
+        self.con.commit()
+        return self.cursor.fetchall()
+
+
