c92cb1031c7ff0aa285f2db9b0f543db79365bd5
mxito3 <1477311729@qq.com>
2018-07-24 11:21:33 +0800

    add changes

--- fileUpload/fileUpload.py
+++ fileUpload/fileUpload.py
@@ -2,17 +2,20 @@
 # @Author: YP
 # @Date:   2018-07-23 23:45:59
 # @Last Modified by:   YP
-# @Last Modified time: 2018-07-24 00:02:45
+# @Last Modified time: 2018-07-24 11:06:41
 from flask import Flask,request
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
-
+uploadFloader='../'
+app.config['UPLOAD_FLODER']=uploadFloader
 @app.route('/')
 def index():
 	return "Hello World"
 @app.route('/file_upload',methods=['Post'])
 def file_upload():
 	file=request.files['test']
-	file.save('./test2')
+	file.save(os.path.join(app.config['UPLOAD_FLODER'],secure_filename(file.filename)))
 	return "upload success"
 
 
